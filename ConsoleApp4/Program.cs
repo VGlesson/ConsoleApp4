9f52c51fbfbf6a8b2c5f4926ab2c3d2cc69d9a80
saxar <saxar@MSI>
2022-10-18 21:28:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 1;
+            int s = 0;
+            for (int i = 1; i <= n; i++) 
+            {
+                s =s+k;
+                k = k + 2;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
